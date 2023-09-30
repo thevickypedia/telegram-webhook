eb6d03692a56314079864287e7af5b56d3a99359
thevickypedia <vignesh.sivanandarao@gmail.com>
2023-09-30 18:30:14 -0500

    Serialize data more efficiently

--- models/processor.py
+++ models/processor.py
@@ -1,25 +1,24 @@
 import os
 from typing import Dict, Tuple, Union
 
-from models.settings import Message
+from models.settings import Chat, Video, Text, Voice, Audio, Document, PhotoFragment
 
 
 def serialize(payload: Dict[str, Union[str, int, dict]]) -> Tuple[str, int]:
-    base_class = Message(**payload)
-    chat_payload = base_class.Chat(**{**payload['chat'], **payload['from']})
+    chat = Chat(**{**payload, **payload['chat'], **payload['from']})
     if payload.get('video'):
-        return process_video(chat_payload, base_class.Video(**payload['video']))
+        return process_video(chat, Video(**payload['video']))
     if payload.get('text'):
-        return process_text(chat_payload, base_class.Text(**payload))
+        return process_text(chat, Text(**payload))
     if payload.get('voice'):
-        return process_voice(chat_payload, base_class.Voice(**payload['voice']))
+        return process_voice(chat, Voice(**payload['voice']))
     if payload.get('audio'):
-        return process_audio(chat_payload, base_class.Audio(**payload['audio']))
+        return process_audio(chat, Audio(**payload['audio']))
     if payload.get('document'):
-        return process_document(chat_payload, base_class.Document(**payload['document']))
+        return process_document(chat, Document(**payload['document']))
     if payload.get('photo'):
         # Matches for compressed images
-        return process_photo(chat_payload, [base_class.PhotoFragment(**d) for d in payload['photo']])
+        return process_photo(chat, [PhotoFragment(**d) for d in payload['photo']])
     raise ValueError("Payload type is not allowed.")
 
 
@@ -27,7 +26,7 @@ def process_video(chat, data) -> Tuple[str, int]:
     return "Received a video", chat.id
 
 
-def process_text(chat: Message.Chat, data: Message.Text) -> Tuple[str, int]:
+def process_text(chat: Chat, data: Text) -> Tuple[str, int]:
     if 'stop' in data.text or 'exit' in data.text or 'kill' in data.text:
         response = "Stopping webhook server"
         os.kill(os.getpid(), 15)  # Send a terminate signal for the current process ID triggering shutdown event
